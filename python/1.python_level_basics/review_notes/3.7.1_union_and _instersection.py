5df72228983f086c77362ea939b83809785dcb3a
luisnogueira <luis.nogueira@siemens.com>
2022-10-02 00:29:17 +0100

    add new files 01102022

--- python/1.python_level_basics/review_notes/3.7.1_union_and _instersection.py
+++
@@ -1,8 +0,0 @@
-letterA = {"A","B", "C", "D", "E"}
-letterB = {"A", "D","E", "F"}
-union = letterA | letterB
-intersection = letterA & letterB
-difference = letterA - letterB
-print(f"union = {union}")
-print(f"Intersection = {intersection}")
-print(f"difference = {difference}")
